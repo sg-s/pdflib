696ea398e24bf2c60550e8f5ab87c8d58a665c8b
Srinivas Gorur-Shandilya <code@srinivas.gs>
2021-10-11 13:06:51 -0400

    added setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='pdflib',
+    version='0.0.1',
+    packages=['pdflib'],
+    include_package_data=True,
+    description='pretty printing of PDFs from Jupyter notebooks',
+    url='https://github.com/inscopix/astellas-ACC',
+    author='Srinivas Gorur-Shandilya',
+    author_email='code@srinivas.gs',
+)
